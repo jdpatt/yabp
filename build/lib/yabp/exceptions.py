69bba6fdb35d45c5c1f44edac268d15414e27b9f
David Patterson <david.patterson@plexus.com>
2022-07-01 10:42:32 -0400

    Fix gitignore to ignore build/

--- build/lib/yabp/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""Exceptions for yabp."""
-
-
-class CommandError(Exception):
-    """A command failed to either execute or return a response."""
-
-
-class DeviceError(Exception):
-    """The device failed to response correctly."""
